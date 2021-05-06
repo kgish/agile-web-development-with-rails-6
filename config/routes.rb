3d3ba552e92bed0bc7214cfbdee8ecfe39424c19
Kiffin Gish <kiffin.gish@planet.nl>
2021-05-06 20:37:34 +0200

    Iteration C1: Creating the Catalog Listing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'store#index', as: 'store_index'
+  # get 'store/index'
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
