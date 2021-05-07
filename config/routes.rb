745de282dcb73eb2be8ec9bc9649f013678717cb
Kiffin Gish <kiffin.gish@planet.nl>
2021-05-07 21:15:12 +0200

    Iteration D1: Finding a Cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :carts
   root 'store#index', as: 'store_index'
   # get 'store/index'
   resources :products
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_135938) do
+ActiveRecord::Schema.define(version: 2021_05_07_191018) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
